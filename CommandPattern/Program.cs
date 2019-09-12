a05b7f2512dd838dcfea84598238b91dfdb8cac7
Caspar van Hoffe <caspar@logiqs.nl>
2019-09-12 13:50:45 +0200

    Added CommandPattern

--- CommandPattern/Program.cs
+++ CommandPattern/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace CommandPattern
 {
@@ -9,26 +10,30 @@ namespace CommandPattern
             SimpleRemoteControl remote = new SimpleRemoteControl();
             Light light = new Light();
             LightOnCommand lightOn = new LightOnCommand(light);
+            GarageDoor garageDoor = new GarageDoor();
+            GarageDoorOpenCommand garageDoorOpen = new GarageDoorOpenCommand(garageDoor);
 
             remote.SetCommand(lightOn);
+            remote.ButtonWasPressed();
+            remote.SetCommand(garageDoorOpen);
             remote.ButtonWasPressed();
         }
     }
 
-    public interface Command
+    public interface ICommand
     {
         void Execute();
     }
 
     public class SimpleRemoteControl
     {
-        private Command slot;
+        private ICommand slot;
 
         public SimpleRemoteControl()
         {
         }
 
-        public void SetCommand(Command command)
+        public void SetCommand(ICommand command)
         {
             this.slot = command;
         }
@@ -39,7 +44,60 @@ namespace CommandPattern
         }
     }
 
-    public class LightOnCommand : Command
+    public class RemoteControl
+    {
+        private ICommand[] onCommands;
+        private ICommand[] offCommands;
+
+        public RemoteControl()
+        {
+            onCommands = new ICommand[7];
+            offCommands = new ICommand[7];
+
+            ICommand noCommand = new NoCommand();
+            for (int i = 0; i < onCommands.Length; i++)
+            {
+                onCommands[i] = noCommand;
+                offCommands[i] = noCommand;
+            }
+        }
+
+        public void Setcommand(int slot, ICommand onCommand, ICommand offCommand)
+        {
+            onCommands[slot] = onCommand;
+            offCommands[slot] = offCommand;
+        }
+
+        public void OnButtonWasPushed(int slot)
+        {
+            onCommands[slot].Execute();
+        }
+        public void OffButtonWasPushed(int slot)
+        {
+            offCommands[slot].Execute();
+        }
+
+        //public string ToString()
+        //{
+        //    StringBuilder stringBuilder = new StringBuilder();
+        //    stringBuilder.Append("\n------ Remote Control ------\n");
+        //    for (int i = 0; i < onCommands.Length; i++)
+        //    {
+        //        stringBuilder.Append($"[slot {i}] {onCommands[i].getClass().getName()}")
+        //    }
+        //}
+
+    }
+
+    public class NoCommand : ICommand
+    {
+        public void Execute()
+        {
+            Console.WriteLine("Nothing happened...");
+        }
+    }
+
+    public class LightOnCommand : ICommand
     {
         private Light light;
 
@@ -48,10 +106,30 @@ namespace CommandPattern
             this.light = light;
         }
 
+        //#region ICommand Members
         public void Execute()
         {
             this.light.On();
         }
+
+        //#endregion
+    }
+
+    public class GarageDoorOpenCommand : ICommand
+    {
+        private GarageDoor garageDoor;
+
+        public GarageDoorOpenCommand(GarageDoor garageDoor)
+        {
+            this.garageDoor = garageDoor;
+        }
+
+        //#region ICommand Members
+        public void Execute()
+        {
+            this.garageDoor.Up();
+        }
+        //#endregion
     }
 
     public class Light
@@ -64,6 +142,34 @@ namespace CommandPattern
         public void Off()
         {
             Console.WriteLine("Lights off");
+        }
+    }
+
+    public class GarageDoor
+    {
+        public void Up()
+        {
+            Console.WriteLine("Garage door opening...");
+        }
+
+        public void Down()
+        {
+            Console.WriteLine("Garage door closing...");
+        }
+
+        public void Stop()
+        {
+            Console.WriteLine("Garage door stops");
+        }
+
+        public void LightOn()
+        {
+            Console.WriteLine("Garage light on");
+        }
+
+        public void LightOff()
+        {
+            Console.WriteLine("Garage light off");
         }
     }
 }
